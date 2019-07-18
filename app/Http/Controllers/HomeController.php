74f3068cd1f269c088550126c3a10eba41b069b6
Leonardino80 <gilgamesh2005@gmail.com>
2019-07-18 03:07:21 +0200

    creazione, creazione database phpmyadmin, auth

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+      // e' un vincolo per poter utilizzare tutte le funzioni che ci son qua dentro
+      // devi essere autenticato per usare index
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
